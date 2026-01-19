23dea88b07c1c855d14f668954056e4b8e02f8c3
Matt Lyons (Laptop) <109154359+MLphysics@users.noreply.github.com>
2026-01-19 03:47:34 +0000

    peakfinder

--- 2_ANALYSIS_FITTING TRANSMISSION.py
+++ 2_ANALYSIS_FITTING TRANSMISSION.py
@@ -10,7 +10,7 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 
-
+from scipy.signal import find_peaks
 
 # Band definition
 
@@ -43,7 +43,7 @@ def resample(df: pd.DataFrame, nf: np.ndarray) -> pd.DataFrame:
 
 
 def cleandata(df: pd.DataFrame) -> pd.DataFrame:
-    data_smoothed = df["data"].rolling(window=3, min_periods=1).mean()
+    data_smoothed = df["data"].rolling(window=1, min_periods=1).mean()
     return pd.DataFrame({
         "freq": df["freq"],
         "data": data_smoothed
@@ -74,6 +74,24 @@ def savedata(newpath: Path,
     filename = f"{band}_trans_{angle:.1f}deg_{material}_{date}.txt"
     df.to_csv(newpath / filename, sep=",", index=False)
 
+def fit_peak_quadratic(x, y, idx, window=5):
+    #Fits a quadratic to points around a peak and returns interpolated peak position and height.
+    
+    i0 = max(idx - window, 0)
+    i1 = min(idx + window + 1, len(x))
+
+    xloc = x[i0:i1]
+    yloc = y[i0:i1]
+
+    # Fit y = ax^2 + bx + c
+    a, b, c = np.polyfit(xloc, yloc, 2)
+
+    # Vertex of parabola
+    x_peak = -b / (2 * a)
+    y_peak = c - b**2 / (4 * a)
+
+    return x_peak, y_peak
+
 
 def main(pathref, scan, material, date, SUBFOLD, band):
 
@@ -101,9 +119,9 @@ def main(pathref, scan, material, date, SUBFOLD, band):
     angle_arr = []
     avg_trans_dB = []
 
-
+    ###################
     # Transmission vs frequency
-
+    ###################
     plt.figure(figsize=(9, 6))
     plt.title("Transmission vs Frequency")
 
@@ -140,29 +158,79 @@ def main(pathref, scan, material, date, SUBFOLD, band):
     plt.legend(title="Angle", ncol=2, fontsize=8)
     plt.tight_layout()
     plt.show()
+    ###################
+    # Transmission Vs Frequency end
+    ###################
 
-
-    # Average transmission vs angle
-
+    # order the data
     angle_arr = np.array(angle_arr)   ### make sure this is arr not strings 
     avg_trans_dB = np.array(avg_trans_dB)
+    idx_ang = np.argsort(angle_arr)
+    angle_arr    = angle_arr[idx_ang]
+    avg_trans_dB = avg_trans_dB[idx_ang]
 
-   
-    idx = np.argsort(angle_arr)  # Sort by angle
+    ###################
+    ### finding peaks 
+    ###################
+    # angle_arr, avg_trans_dB
+    peaks, props = find_peaks(
+        avg_trans_dB,
+        prominence=0.2,     # dB 
+        distance=10         # minimum # of points between peaks
+        )
+
+    peak_ang  = angle_arr[peaks]
+    peak_vals = avg_trans_dB[peaks]
+
+    # interpolation 
+    peak_ang_interp = []
+    peak_vals_interp = []  ## peak oositions
+
+    for idx in peaks:
+        xp, yp = fit_peak_quadratic(angle_arr, avg_trans_dB, idx, window=5) # I.d. your peaks
+        peak_ang_interp.append(xp)
+        peak_vals_interp.append(yp) # put them in the array for plotting
+    
+    peak_ang_interp = np.array(peak_ang_interp)
+    peak_vals_interp = np.array(peak_vals_interp)## MAKE SURE THEYRE AN ARRAY OTHERWISE IT CRASHES 
+    
+    ###################
+    ### peaks found 
+    ###################
+
+    ###################
+    # Average transmission vs angle plot start
+    ###################
+
+
 
     plt.figure(figsize=(7, 5))
     plt.title("Average Transmission vs Angle")
+    
+    plt.scatter(peak_ang , peak_vals, marker = "x", c= "r", label="peaks") ### add peaks to our plot
+    plt.scatter(peak_ang_interp, peak_vals_interp, marker = "x", c= "black", alpha= 0.3, label="peaks from quadratic") ### add peaks to our plot
+    
     plt.plot(
-        angle_arr[idx],
-        avg_trans_dB[idx],
+        angle_arr,
+        avg_trans_dB,
         marker="o",
+        ms=3,
         linestyle="-"
     )
+    
+    
+    
     plt.xlabel("Angle (deg)")
     plt.ylabel("Average Transmission (dB)")
     plt.grid(True)
     plt.tight_layout()
+    plt.legend()
     plt.show()
+    ###################
+    # Average transmission vs angle plot start
+    ###################
+    
+
 
 
 
