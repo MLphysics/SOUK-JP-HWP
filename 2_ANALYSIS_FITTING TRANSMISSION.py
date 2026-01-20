e8ad77f5fa79031b452e2e963f6aaf082744dae4
Matt Lyons (Laptop) <109154359+MLphysics@users.noreply.github.com>
2026-01-20 07:47:33 +0000

    MORE DATA!

--- 2_ANALYSIS_FITTING TRANSMISSION.py
+++ 2_ANALYSIS_FITTING TRANSMISSION.py
@@ -136,7 +136,7 @@ def process_scan(sample_paths, dfref, freq_common, TRANS_SCALE,
                 for i in seg_peaks:
                     quad_fits.append(fit_peak_quadratic(
                         angles_sorted, avg_trans_dB_sorted, i,
-                        window=max(4, len(seg_idx)//4)
+                        window=max(3, len(seg_idx)//4)
                     ))
         else:
             for i in peaks:
@@ -360,9 +360,9 @@ def main(scan, material, date, SUBFOLD, band,
 # RUN
 # =====================
 main(
-    scan="MF2_3_rotation",
-    material="sapphire_MF2",
-    date="20260119",
+    scan="MF3_rotation_center",
+    material="sapphire_MF3",
+    date="20260120",
     SUBFOLD="MLOG",
     band=Fband,
     INTERPPEAKS=True,
